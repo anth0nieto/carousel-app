0c4e462e4fcc96159b71dddd69c99cc4ff46937b
Anthony Montero <antho.montero@gmail.com>
2021-09-19 15:47:46 -0400

    Adding icon and splash image

--- babel.config.js
+++ babel.config.js
@@ -2,11 +2,7 @@ module.exports = api => {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    env: {
-      production: {
-        plugins: ['react-native-paper/babel'],
-      },
-    },
+    env: {},
     plugins: [
       [
         require.resolve('babel-plugin-module-resolver'),
